9aa42af822a99938bece17eeb53d13fe682bd22b
dopemmanuel <dopemmanuel@iesrafaelalberti.es>
2023-10-26 12:21:15 -0400

    Edit

--- src/Ejercicio_2.1.1.py
+++ src/Ejercicio_2.1.1.py
@@ -12,4 +12,21 @@ elif edad > 18:
     print("Eres mayor de edad")
 """
 
-def
+def verificar_edad():
+    while True:
+        try:
+            edad = int(input("Ingrese su edad: "))
+
+#Esta es opcional XD
+            if edad > 100:
+                print("En serio??!!", edad, "años??, tu deverias de estar muerto eso es imposible...")
+        
+            elif  edad < 18:
+                print("Eres menor de edad")
+            else:
+                print("Eres mayor de edad")
+            break
+        except ValueError:
+            print("Por Favor, Ingrese una edad valida")
+
+verificar_edad()
