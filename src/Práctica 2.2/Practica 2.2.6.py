dd7688ccd0c38e5f14153e26c12b250db1e84346
dopemmanuel <148891292+dopemmanuel@users.noreply.github.com>
2023-11-04 15:59:14 -0400

    last one??

--- src/Práctica 2.2/Practica 2.2.6.py
+++ src/Práctica 2.2/Practica 2.2.6.py
@@ -1,14 +1,19 @@
 def tabla_de_multiplicar():
-    num1 = int(input("Ingrese el multiplicando(factor): "))
-    num2 = int(input("Ingrese el multiplicador: "))
-
-if num1:
-
-    for i in range(1,11):
+    num1 = input("Ingrese el multiplicando(factor): ")
 
 
-
-
+    if num1:
+        num1 = int(num1)
+        print("Tabla de multiplicar del", num1)
+        for i in range(1,11):
+            resultado = num1 * i
+            print("", num1, "x", i,"=", resultado,)
+        else:
+            for i in range(1,11):
+                print("Tabla de multiplicar del", i)
+                for k in range(1,11):
+                    resultado = i * k
+                print("", i,"x", k,"=",resultado)
 
 def main():
     print("Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.")
