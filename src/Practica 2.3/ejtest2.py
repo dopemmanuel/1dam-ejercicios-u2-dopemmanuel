fd99de7bd7381f0067fa87747338fe777c03afa9
Eolm728 <dopemmanuel@iesrafaelalberti.es>
2023-11-07 19:57:38 +0100

    *

---
+++ src/Practica 2.3/ejtest2.py
@@ -0,0 +1,20 @@
+def grados_a_fahrenheit():
+    while True:
+        try:
+            
+
+
+            celsius = float(input("Ingrese los grados celcius: "))
+
+            fahrenheit =  celsius * 9/5 + 32
+            print("La temperatura de celcius a fharenheit es:", fahrenheit )
+            break
+        except ValueError:
+            print("ERROR, no son los datos que esperabamos")
+
+def main():
+    print("Reescribe el programa conversor de temperaturas para que lea repetidamente la temperatura hasta que sea correcta, debe detectar los fallos usando try y except:")
+    grados_a_fahrenheit()
+if __name__=="__main__":
+    main()
+
