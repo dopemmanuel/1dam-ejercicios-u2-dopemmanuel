85a0a508dbe08c65f2b2766ed84b58e7f599024c
Eolm728 <dopemmanuel@iesrafaelalberti.es>
2023-11-07 19:12:59 +0100

    *

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-# Acabo de entender que el main es el titulo en salida para lo que acabas de programar... hice pruebas y es facinante:
-def main():
-    print("Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.")
-#Aqui Coloque la funcion de lo que programe y el print que esta arriba sirve como titulo:
-if __name__ == "__main__":
-    main()
--- tests/PruebaDeAprendizaje.py
+++ tests/PruebaDeAprendizaje.py
@@ -5,4 +5,10 @@ veces_a_contar = int(input("Ingresa las veces quue quieres que se cuente:"))
 
 for i in range():
     print()
-"""    
+"""
+# Acabo de entender que el main es el titulo en salida para lo que acabas de programar... hice pruebas y es facinante:
+def main():
+    print("Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas.")
+#Aqui Coloque la funcion de lo que programe y el print que esta arriba sirve como titulo:
+if __name__ == "__main__":
+    main()
