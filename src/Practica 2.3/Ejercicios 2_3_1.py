bbb8ba17841c8f6ac9e2d540b26fe33bf49f1518
Eolm728 <dopemmanuel@iesrafaelalberti.es>
2023-11-07 20:05:18 +0100

    *

---
+++ src/Practica 2.3/Ejercicios 2_3_1.py
@@ -0,0 +1,13 @@
+def pedir_edad():
+
+
+
+
+
+
+
+    def main():
+        print("Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad): ")
+    pedir_edad()
+if __name__=="__main__":
+    main()
