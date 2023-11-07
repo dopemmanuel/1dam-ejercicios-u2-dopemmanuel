5231fe0e90709ab92ceeca813bf5fd6905d08db2
dopemmanuel <148891292+dopemmanuel@users.noreply.github.com>
2023-11-07 12:58:27 -0400

    U

--- tests/PruebaDeAprendizaje.py
+++ tests/PruebaDeAprendizaje.py
@@ -1,6 +1,30 @@
+"""
 #Esto es para comprender mejor lo de "range" ignoralo:
 num = str(input("Dame un numero: "))
 veces_a_contar = int(input("Ingresa las veces quue quieres que se cuente:"))
 
-for i in range(num):
-    print(veces_a_contar)
+for i in range():
+    print()
+"""
+
+def division():
+    num1 = int(input("Dividendo: "))
+    num2 = int(input("Divisor: "))
+    
+    if num2 == 0:
+        raise ValueError("ERROR La división entre cero es imposible...")
+    elif num1 == 0:
+        raise ValueError(": La división entre cero es imposible...")
+    
+    div = num1 / num2
+    print("Su resultado es:", div)
+
+try:
+    def main():
+        print("Division ")
+        division()
+    if __name__=="__main__":
+        main()
+except ValueError as y:
+    print(y)
+    
