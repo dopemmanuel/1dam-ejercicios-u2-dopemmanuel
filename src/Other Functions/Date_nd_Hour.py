9b44338cc9172d072368059bcc26503274ae3b3f
Eolm728 <dopemmanuel@iesrafaelalberti.es>
2023-11-09 18:29:55 +0100

    **

--- src/Other Functions/Date_nd_Hour.py
+++ src/Other Functions/Date_nd_Hour.py
@@ -1,17 +1,6 @@
 
 """
-import time
-import sys
-
-while True:
-    tiempo_anterior = ""
-    while True:
-        tiempo_actual = time.strftime("%H:%M:%S")
-        if tiempo_actual != tiempo_anterior:
-            sys.stdout.write("\r" + tiempo_actual)
-            sys.stdout.flush()
-            tiempo_anterior = tiempo_actual
-        time.sleep(1)
+()^_____^)
 """
 import time
 while True:
