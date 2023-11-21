1d4edeb285e376378b537ca0a1c47cc594379015
Eolm728 <dopemmanuel@iesrafaelalberti.es>
2023-11-21 19:32:31 +0100

    ┗|｀O′|┛

--- src/Practica 2.4/ej.py
+++ src/Practica 2.4/ej.py
@@ -1,14 +1,21 @@
-def palabra() -> tuple:
+def obtener_palabra() -> tuple:
     wrd = tuple(input("Dame una palabra: "))
     return wrd
 
+def contar_vocales(wrd: tuple) -> tuple:
+    vocales = [("a", 0), ("e", 0), ("i", 0), ("o", 0), ("u", 0)]
 
-def vocales(wrd: tuple) -> tuple:
-    vocal = tuple(["a",0],["e",0],["i",0],["o",0],["u",0])
-    for vocale in vocal:
-        vocale[1] = palabra.count(vocal[0])
-        return vocal
+    for letra in wrd:
+        for vocal in vocales:
+            if letra.lower() == vocal[0]:
+                vocal = (vocal[0], vocal[1] + 1)
+
+    return vocales
+
 def main():
-    wrd = palabra()
-    vocal = vocales()
-    print
+    wrd = obtener_palabra()
+    resultado_vocales = contar_vocales(wrd)
+    print("Resultado de contar vocales:", resultado_vocales)
+
+if __name__ == "__main__":
+    main()
